331998132e4599e58c625fbaf6935da386a7af28
jiangzg <2992265870@qq.com>
2017-12-20 10:54:38 +0800

    parceljs实践

---
+++ src/parcel/app.vue
@@ -0,0 +1,22 @@
+<template>
+  <div @click="change">
+    {{ msg }}
+  </div>
+</template>
+<script>
+	export default {
+		data() {
+			return {
+				msg: 'app'
+			}
+		},
+		methods: {
+			change() {
+				this.msg = Math.random()
+			}
+		}
+	}
+</script>
+<style>
+
+</style>
